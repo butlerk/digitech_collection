759ca2eb60350feea4d70761ddd90e4f75dffbfe
Kelly Butler <butlerk@bhs.vic.edu.au>
2021-09-08 21:19:45 +1000

    Added base html files

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,19 @@ from app import app
 def index():
     return render_template('index.html', title = 'Home')
 
+@app.route('/loans')
+def loans():
+    return render_template('loans.html', title = 'Loans')
 
-@app.route('/helloworld')
-def helloworld():
-    return render_template('helloworld.html', title = 'HelloWorld')
+@app.route('/reports')
+def reports():
+    return render_template('reports.html', title = 'Reports')
+
+@app.route('/locations')
+def locations():
+    return render_template('locations.html', title = 'Locations')
+
+@app.route('/users')
+def users():
+    return render_template('users.html', title = 'Users')
 
