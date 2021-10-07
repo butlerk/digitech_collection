2ae167b3d0f93dfba1b129eef1c6fa038f65f241
Kelly Butler <butlerk@bhs.vic.edu.au>
2021-10-07 18:52:58 +1100

    change config not to track modification

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 app.config['SECRET_KEY'] = 'example-secret' 
 
